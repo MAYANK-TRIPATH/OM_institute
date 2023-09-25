74b35576826ab8004966411c4e3dd9e2494805e3
Mayank Tripathi <84989130+MAYANK-TRIPATH@users.noreply.github.com>
2023-09-25 22:49:19 +0530

    Init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from './components/Navbar';
+import About from './pages/About'; 
+import Banner from './pages/Banner'; 
+
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <>
+    <Navbar />
+    <Banner />
+    <About />
+    
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/CardT.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CardT = () => {
+  return (
+    <div>CardT</div>
+  )
+}
+
+export default CardT
